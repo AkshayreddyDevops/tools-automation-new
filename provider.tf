3c99a96f31e407a3e649544adfdf961f28e2b04a
akga0822 <akshay.gaddam@netcracker.com>
2024-12-05 19:44:38 +0530

    gethub-runner

--- provider.tf
+++ provider.tf
@@ -1,9 +1,24 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
+# provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+      region = "us-east-1"
+    }
+     helm = {
+       kubernetes = {
+        config_path = "~/.kube/config"
+       }
+     }
   }
 }
--- state.tf
+++ state.tf
@@ -4,11 +4,4 @@ terraform {
     key = "tools/terraform.tfstate"
     region = "us-east-1"
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-  
 }
