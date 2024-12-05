013aae44e8cdb259bd0aa40ed5ce647d0664d7b0
akga0822 <akshay.gaddam@netcracker.com>
2024-12-05 19:53:54 +0530

    gethub-runner

--- state.tf
+++ state.tf
@@ -1,28 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tools-automation-new"
-    key = "tools/terraform.tfstate"
+    key    = "tools/terraform.tfstate"
     region = "us-east-1"
-  }
-}
-
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-      region = "us-east-1"
-    }
-     helm = {
-       kubernetes = {
-        config_path = "~/.kube/config"
-       }
-     }
-    s3 = {
-    bucket = "tools-automation-new"
-    key = "tools/terraform.tfstate"
-    region = "us-east-1"
-  }     
   }
 }
