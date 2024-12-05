beb5e4ab8a28c198fe611c44ec1f77a23931b5f4
akga0822 <akshay.gaddam@netcracker.com>
2024-12-05 14:15:32 +0530

    gethub-runner

--- variables.tf
+++ variables.tf
@@ -9,25 +9,25 @@ variable "tools" {
       ports = {}
     }
 
-    vault = {
-      name = "vault"
-      instance_type = "t3.small"
-      port_no = {
-        vault = 8200
-      }
-      policy_action = []
-      volume_size = 20
-    }
+    # vault = {
+    #   name = "vault"
+    #   instance_type = "t3.small"
+    #   port_no = {
+    #     vault = 8200
+    #   }
+    #   policy_action = []
+    #   volume_size = 20
+    # }
 
-    minikube = {
-      name = "minikube"
-      instance_type = "t3.medium"
-      port_no = {
-        vault = 8443
-      }
-      policy_action = []
-      volume_size = 30      
-    }
+    # minikube = {
+    #   name = "minikube"
+    #   instance_type = "t3.medium"
+    #   port_no = {
+    #     vault = 8443
+    #   }
+    #   policy_action = []
+    #   volume_size = 30      
+    # }
   }
 }
 
