b7aad273ede621b14addaacd08643d4712df18df
akga0822 <akshay.gaddam@netcracker.com>
2024-12-07 11:52:31 +0530

    gethub-runner

--- call-setup-tools-ansible.sh
+++ call-setup-tools-ansible.sh
@@ -1,1 +1,1 @@
-ansibke-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321  -e tool_name setup-tools-ansible.yml
+ansible-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321  -e tool_name setup-tools-ansible.yml
