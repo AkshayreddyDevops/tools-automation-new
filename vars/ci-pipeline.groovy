1f126536ecf6d73def96ca91ccd7e93a2062a6c2
akga0822 <akshay.gaddam@netcracker.com>
2024-12-17 19:41:01 +0530

    jenkins

---
+++ vars/ci-pipeline.groovy
@@ -0,0 +1,26 @@
+def call(){
+  node(){
+  if( env.TAG_NAME){
+  stage('Docker Build'){
+    print 'Docker Build'
+  }
+  stage('Docker Push'){
+    print 'Docker Push'
+  }
+  stage('Deploy to Dev'){
+    print 'Deploy to Dev'
+   }
+  }
+  else {
+  stage('compile'){
+    print 'compile'
+  }
+  if(env.BRANCH_NAME != 'main') {
+    stage('Test Cases'){
+    print 'Test Cases'
+   }
+  }
+  
+ }
+}
+}
