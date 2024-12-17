13cd53945004830369787921d57cacd783bd6df3
akga0822 <akshay.gaddam@netcracker.com>
2024-12-17 19:17:50 +0530

    jenkins

---
+++ roles/jenkins/templates/shared-library.groovy
@@ -0,0 +1,19 @@
+import jenkins.model.*;
+import org.jenkinsci.plugins.workflow.libs.*;
+import jenkins.scm.api.SCMSource;
+
+SCMSource scm = new jenkins.plugins.git.GitSCMSource("https://github.com/AkshayreddyDevops/tools-automation-new.git")
+scm.setCredentialsId("")
+
+LibraryRetriever libRetriever = new SCMSourceRetriever(scm)
+
+LibraryConfiguration libConfig = new LibraryConfiguration("roboshop-jenkins-library", libRetriever)
+libConfig.setDefaultVersion("main")
+libConfig.setImplicit(false)
+libConfig.setIncludeInChangesets(true)
+
+List<LibraryConfiguration> libraries= new ArrayList<LibraryConfiguration>()
+libraries.add(libConfig)
+
+GlobalLibraries globalLibs = GlobalConfiguration.all().get(GlobalLibraries.class)
+globalLibs.setLibraries(libraries)
